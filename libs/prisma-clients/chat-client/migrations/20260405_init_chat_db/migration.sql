edfbcd3e35cbbb2a6a4c364d209eade4cc3d285a
TungPham45 <tungp788@gmail.com>
2026-04-06 17:30:26 +0700

    constraint get exist

--- libs/prisma-clients/chat-client/migrations/20260405_init_chat_db/migration.sql
+++ libs/prisma-clients/chat-client/migrations/20260405_init_chat_db/migration.sql
@@ -1,25 +1,25 @@
 -- CreateTable
-CREATE TABLE "Conversation" (
+CREATE TABLE "conversations" (
     "id" SERIAL NOT NULL,
     "buyerId" INTEGER NOT NULL,
     "sellerId" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Conversation_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "conversations_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Message" (
+CREATE TABLE "messages" (
     "id" SERIAL NOT NULL,
     "conversationId" INTEGER NOT NULL,
     "senderId" INTEGER NOT NULL,
     "content" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "messages_pkey" PRIMARY KEY ("id")
 );
 
 -- AddForeignKey
-ALTER TABLE "Message" ADD CONSTRAINT "Message_conversationId_fkey" FOREIGN KEY ("conversationId") REFERENCES "Conversation"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "messages" ADD CONSTRAINT "messages_conversationId_fkey" FOREIGN KEY ("conversationId") REFERENCES "conversations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
